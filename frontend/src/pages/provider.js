89c363406b2257256b5c17112c7c40179189357d
karasakalmt <karasakal99@gmail.com>
2022-10-01 19:55:54 +0300

    minting

--- frontend/src/pages/provider.js
+++ frontend/src/pages/provider.js
@@ -1,4 +1,25 @@
+import { useState } from "react"
+import { mintCertificate } from "../utils/web3/certificate";
+
 const Provider = () => {
+  const [amount, setAmount] = useState('');
+  const [certificate, setCertificate] = useState('');
+
+
+  const amountChange = (e) => {
+    setAmount(e.target.value);
+  }
+
+  const certificateChange = (e) => {
+    setCertificate(e.target.value);
+  }
+
+  const mint = async() => {
+    await mintCertificate(parseInt(amount), parseInt(certificate));
+    setAmount('');
+    setCertificate('');
+  }
+
   return (
     <section className="w-full h-screen relative flex flex-col justify-center items-center">
       <div className="container w-full flex items-center justify-center">
@@ -13,6 +34,7 @@ const Provider = () => {
             className="block p-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="1000000.0"
             required
+            onChange={(e) => amountChange(e)}
             />
             <p className="pt-5">Certificate ID</p>
             <input
@@ -20,8 +42,10 @@ const Provider = () => {
             className="block p-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="12345678"
             required
+            onChange={(e) => certificateChange(e)}
             />
             <button
+              onClick={mint}
               className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg align-center w-full mt-5"
             >Mint NFT</button>
           </div>
