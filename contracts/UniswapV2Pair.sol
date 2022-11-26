f4b89b9ae71e13b49d49dbfe39e14c1584a0a15f
karasakalmt <karasakal99@gmail.com>
2022-11-26 22:27:28 +0200

    version fixes and renewing the interfaces

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.4;
 
 import './interfaces/IUniswapV2Pair.sol';
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import './libraries/Math.sol';
 import './libraries/UQ112x112.sol';
-import './interfaces/IERC20.sol';
 import './interfaces/IUniswapV2Factory.sol';
 import './interfaces/IUniswapV2Callee.sol';
 
