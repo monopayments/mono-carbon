f110937d6e677db1e12b98e04815c42f9687e58a
karasakalmt <karasakal99@gmail.com>
2022-11-28 22:40:49 +0200

    version optimizations

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import './interfaces/IUniswapV2Pair.sol';
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import './interfaces/IUniswapV2Pair.sol';
+import './UniswapV2ERC20.sol';
 import './libraries/Math.sol';
 import './libraries/UQ112x112.sol';
 import './interfaces/IUniswapV2Factory.sol';
 import './interfaces/IUniswapV2Callee.sol';
 
-contract UniswapV2Pair is IUniswapV2Pair, ERC20 {
+contract UniswapV2Pair is UniswapV2ERC20, IUniswapV2Pair {
     using SafeMath  for uint;
     using UQ112x112 for uint224;
 
@@ -47,19 +47,7 @@ contract UniswapV2Pair is IUniswapV2Pair, ERC20 {
         require(success && (data.length == 0 || abi.decode(data, (bool))), 'UniswapV2: TRANSFER_FAILED');
     }
 
-    event Mint(address indexed sender, uint amount0, uint amount1);
-    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
-    event Swap(
-        address indexed sender,
-        uint amount0In,
-        uint amount1In,
-        uint amount0Out,
-        uint amount1Out,
-        address indexed to
-    );
-    event Sync(uint112 reserve0, uint112 reserve1);
-
-    constructor() public {
+    constructor() {
         factory = msg.sender;
     }
 
@@ -72,7 +60,8 @@ contract UniswapV2Pair is IUniswapV2Pair, ERC20 {
 
     // update reserves and, on the first call per block, price accumulators
     function _update(uint balance0, uint balance1, uint112 _reserve0, uint112 _reserve1) private {
-        require(balance0 <= uint112(-1) && balance1 <= uint112(-1), 'UniswapV2: OVERFLOW');
+        // solidity 0.8 compiler automatically checks for overflow and underflows
+        // require(balance0 <= uint112(-1) && balance1 <= uint112(-1), 'UniswapV2: OVERFLOW');
         uint32 blockTimestamp = uint32(block.timestamp % 2**32);
         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
         if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {
